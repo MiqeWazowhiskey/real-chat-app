d1ccad877264febd6554dc96ad81bc1714ba6758
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-01-24 03:54:36 +0300

    styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,9 +11,11 @@ const Login = () => {
     }
   return (
     <Layout>
+      <div className='w-full h-full flex items-center justify-center'>
       <span>
         <button className='text-2xl font-bold text-[#FFFFFF] rounded-[50px] bg-[#9D68FF] p-5' onClick={login}>Login With Google</button>
       </span>
+      </div>
     </Layout>
   )
 }
