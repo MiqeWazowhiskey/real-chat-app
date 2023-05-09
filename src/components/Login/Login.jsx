1b84df0a9f5d7b77ecdf765b317c3d74143f6dfb
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-05-09 18:03:56 +0300

    feat: change login screen ui

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,37 @@
-import {  signInWithRedirect } from 'firebase/auth'
-import React from 'react'
-import { auth , provider } from '../../firebase'
-import { Layout } from '../Layout'
-
+import { signInWithRedirect } from "firebase/auth";
+import React from "react";
+import { auth, provider } from "../../firebase";
+import { Layout } from "../Layout";
 
 const Login = () => {
-    const login = async() => {
-        signInWithRedirect(auth,provider).catch(er=>alert(er))
-        
-    }
+  const login = async () => {
+    signInWithRedirect(auth, provider).catch((er) => alert(er));
+  };
   return (
     <Layout>
-      <div className='w-full h-full flex items-center justify-center'>
-      <span>
-        <button className='text-2xl font-bold text-[#FFFFFF] rounded-[50px] bg-[#9D68FF] p-5' onClick={login}>Login With Google</button>
-      </span>
+      <div className="w-full h-full flex items-center flex-col justify-center ">
+        <h1 className="text-blue-500 font-bold text-5xl absolute mr-[6px] mb-[6px]">
+          ChatApp
+        </h1>
+        <h1 className="text-[#3f3658] font-bold text-5xl absolute ">ChatApp</h1>
+        <div className="h-5/6 w-full flex items-center flex-col justify-around ">
+          <span>
+            <button
+              className="text-3xl font-semibold text-[#FFFFFF] rounded-md bg-[#DB4437] hover:bg-[#f04b3c] p-5"
+              onClick={login}
+            >
+              Login With Google
+              <i className="fa-brands fa-google-plus-g ml-5"></i>
+            </button>
+          </span>
+          <img
+            src="Hotpot.png"
+            className="object-cover w-[200px] h-[200px] rounded-full"
+          />
+        </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
