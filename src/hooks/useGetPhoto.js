ce001c562463bfbc8c8715b8961b5afadcbef744
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-05-09 14:06:18 +0300

    fix: get photo hook formatting

--- src/hooks/useGetPhoto.js
+++ src/hooks/useGetPhoto.js
@@ -9,15 +9,41 @@ import {
 } from "firebase/storage";
 import { storage } from "../firebase";
 
-export function useGetPhoto(png) {
-  const [uri, setUri] = useState(import.meta.env.VITE_DEFAULT_PNG);
-  getDownloadURL(ref(storage, `images/${png}`))
-    .then((url) => {
-      setUri(url);
-    })
-    .catch(() => {
-      setUri(import.meta.env.VITE_DEFAULT_PNG);
-    });
+export function useGetPhoto(userId) {
+  const [photoUrl, setPhotoUrl] = useState("");
 
-  return uri;
+  useEffect(() => {
+    const pngStorageRef1 = ref(storage, `images/${userId}.png`);
+    const jpegStorageRef1 = ref(storage, `images/${userId}.jpeg`);
+    const pngStorageRef2 = ref(storage, `images/${userId}.PNG`);
+    const jpegStorageRef2 = ref(storage, `images/${userId}.JPEG`);
+
+    const fetchPhotoUrl = async () => {
+      try {
+        const pngUrl = await getDownloadURL(pngStorageRef1);
+        setPhotoUrl(pngUrl);
+      } catch (error) {
+        try {
+          const pngUrl2 = await getDownloadURL(pngStorageRef2);
+          setPhotoUrl(pngUrl2);
+        } catch (error) {
+          try {
+            const jpegUrl1 = await getDownloadURL(jpegStorageRef1);
+            setPhotoUrl(jpegUrl1);
+          } catch (error) {
+            try {
+              const jpegUrl2 = await getDownloadURL(jpegStorageRef2);
+              setPhotoUrl(jpegUrl2);
+            } catch (error) {
+              setPhotoUrl(import.meta.env.VITE_DEFAULT_PHOTO);
+            }
+          }
+        }
+      }
+    };
+
+    fetchPhotoUrl();
+  }, [userId]);
+
+  return photoUrl;
 }
